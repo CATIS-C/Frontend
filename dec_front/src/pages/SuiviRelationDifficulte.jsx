1f86f637f3dcaa6550bd71189e512fdd7a3baad1
CATIS-C <afraebenwadih01@gmail.com>
2025-04-10 14:16:19 +0100

    yyt

---
+++ dec_front/src/pages/SuiviRelationDifficulte.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SuiviRelationDifficulte.css';
+
+const SuiviRelationDifficulte = () => {
+  return (
+    <div className="page-container">
+      <h1>Suivi d’une Relation en Difficulté</h1>
+    </div>
+  );
+};
+
+export default SuiviRelationDifficulte;
